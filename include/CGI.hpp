bf9f410b41a0eadec01bc0c17ae9db733088dfad
yopeng <yopeng@student.42.fr>
2026-06-12 16:21:18 +0200

    CGI execute function

--- include/CGI.hpp
+++ include/CGI.hpp
@@ -1,21 +1,28 @@
 #ifndef CGI_HPP
-#define CGI_HPP
+# define CGI_HPP
+
+# include "Webserv.hpp"
+# include "Config.hpp"
+# include "Request.hpp" 
 
 class CGI
 {
-	private:
-		int		_inputFd;
-		int		_outputFd;
-
 	public:
 		CGI();
 		~CGI();
 
-		int	getOutputFd();
-		int	getInputFd();
+		bool	execute(const Request& request, const LocationConfig& location,
+						const ServerConfig& server, const std::string& resolvePath);
+
+		int		getOutputFd();
+		int		getInputFd();
+	
+	private:
+		int		_inputFd;
+		int		_outputFd;
+
+		CGI(const CGI&);
+		CGI& operator=(const CGI&);
 };
-
-
-
 
 #endif
--- src/CGI.cpp
+++ src/CGI.cpp
@@ -8,6 +8,23 @@ CGI::~CGI()
 {
 }
 
+bool CGI::execute(const Request& request, const LocationConfig& location,
+					const ServerConfig& server, const std::string& resolvePath)
+{
+	std::string absolutePath = resolvePath;
+	if (!resolvePath.empty() && resolvePath[0] != '/')
+	{
+		// current working directory
+		// 4096: common maximum path length in Linux
+		// /home/login/webserv + / +  cgi-bin/script.py -> /home/login/webserv/cgi-bin/script.py
+		char cwd[4096];
+		if (getcwd(cwd, sizeof(cwd)) != 0)
+		    absolutePath = std::string(cwd) + "/" + resolvePath;
+	}
+
+	
+}
+
 int CGI::getInputFd()
 {
 	return (_inputFd);
